fdb0e8e4d99f6d1e6efc4a5c69ebd7ddf056d418
Maria Kazakova <mariakazakova92@gmail.com>
2024-01-11 15:25:52 +0300

    changed header

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,19 +1,31 @@
 import {HEADER_LINKS} from '@component/app/constants/constants';
 import Image from 'next/image';
-import logo from '/public/logo.jpeg'
+import logo from '/public/logoNew.png';
+import styles from './header.module.css';
+import searchIcon from '/public/search.svg'
 
 const Header = () => {
   return (
-    <nav className={'flex justify-between mt-4 items-center'}>
-      <div>
-        <Image src={logo} alt={'logo'} width={'150'} />
+    <nav className={`${styles.headerWrapper} + flex justify-between items-center`}>
+      <div className={styles.header}>
+        <div>
+          <Image src={logo} alt={'logo'} width={'80'} height={'87'}/>
+        </div>
+        <div>
+          <ul className={'flex'}>
+            {HEADER_LINKS.map((link)=>{
+              return <li key={link} className={'mr-10 hover:underline decoration-teal-300 decoration-2'}>{link}</li>
+            })}
+          </ul>
+        </div>
+
+        <div className={'mr-10 w-28 h-8 flex items-center'}>
+          <button className={`${styles.searchIcon} + w-5 h-5 mr-3 rounded-full`}><Image src={searchIcon} width={'6.19'} alt={'search'}/></button>
+          <button className={`${styles.loginButton} + w-16 h-8 border-2 rounded-3xl `}>Login</button>
+
+        </div>
       </div>
-      <ul className={'flex justify-around'}>
-        {HEADER_LINKS.map((link)=>{
-          return <li key={link} className={'mr-10'}>{link}</li>
-        })}
-      </ul>
-      <div className={'mr-10'}>Tel.</div>
+
     </nav>
   )
 }
