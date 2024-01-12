b4b779dbb772e683f388874d8e40e1d448218055
Maria Kazakova <mariakazakova92@gmail.com>
2024-01-12 15:56:35 +0300

    added text to the firstSection

--- src/app/firstSection/FirstSection.tsx
+++ src/app/firstSection/FirstSection.tsx
@@ -4,11 +4,20 @@ import photo from '../../../public/desktop@1x.jpg.png'
 
 const FirstSection = () =>{
   return (
-    <div>
-      <div className={styles.colorBackground}/>
-      <div className={styles.imgBackground}>
-        <Image src={photo} alt={'yoga'} priority width={'fill'}/>
+    <div className={styles.firstSection}>
+      <div className={styles.colorBackground}>
+        <div className={styles.firstSection_text}>
+          <h1>Dein Online-Yogastudio</h1>
+          <p>YogaEasy ist dein persönliches Online-Yogastudio für zu hause oder unterwegs. Mit YogaEasy kannst du ganz bequem Yoga üben - wann und wo immer du willst.</p>
+          <div>
+            <p>25% Rabatt für dich ››</p>
+          </div>
+        </div>
       </div>
+
+        <div className={styles.imgBackground}>
+          <Image src={photo} alt={'yoga'} priority width={'fill'}/>
+        </div>
     </div>
   )
 }
