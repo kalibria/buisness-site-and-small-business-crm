60b7535c4cc15b7f4762100bc5a65833074da087
Maria Kazakova <mariakazakova92@gmail.com>
2024-01-13 21:21:14 +0300

    added a CTAButton

--- src/app/firstSection/FirstSection.tsx
+++ src/app/firstSection/FirstSection.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import CTAButton from '../../uikit/buttons/CTAButton';
 import styles from './firstSection.module.scss';
 import photo from '../../../public/desktop@1x.jpg.png'
 
@@ -9,9 +10,7 @@ const FirstSection = () =>{
         <div className={styles.firstSection_text}>
           <h1 className={'font-philosopher mb-4'}>Dein Online-Yogastudio</h1>
           <p className={'font-source font-bold text-lg mb-11'}>YogaEasy ist dein persönliches Online-Yogastudio für zu hause oder unterwegs. Mit YogaEasy kannst du ganz bequem Yoga üben - wann und wo immer du willst.</p>
-          <div>
-            <p>25% Rabatt für dich ››</p>
-          </div>
+          <CTAButton/>
         </div>
       </div>
 
---
+++ src/uikit/buttons/CTAButton.tsx
@@ -0,0 +1,7 @@
+const CTAButton = () =>{
+  return <button>
+    <p className={'font-source font-semibold'}>25% Rabatt für dich ››</p>
+  </button>
+}
+
+export default CTAButton;
