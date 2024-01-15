f2324e404ad37453a9c997009a2149ef19cdb6e8
Maria Kazakova <mariakazakova92@gmail.com>
2024-01-15 16:33:26 +0300

    added discount div

--- src/app/firstSection/FirstSection.tsx
+++ src/app/firstSection/FirstSection.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image';
-import CTAButton from '../../uikit/buttons/CTAButton';
+import CTAButton from '../../../src/uikit/buttons/CTAButton';
 import styles from './firstSection.module.scss';
 import photo from '../../../public/desktop@1x.jpg.png'
+import discountImg from '../../../public/div.png'
 
 const FirstSection = () =>{
   return (
@@ -11,6 +12,9 @@ const FirstSection = () =>{
           <h1 className={'font-philosopher mb-4'}>Dein Online-Yogastudio</h1>
           <p className={'font-source font-bold text-lg mb-11'}>YogaEasy ist dein persönliches Online-Yogastudio für zu hause oder unterwegs. Mit YogaEasy kannst du ganz bequem Yoga üben - wann und wo immer du willst.</p>
           <CTAButton/>
+        </div>
+        <div className={styles.discountImg}>
+          <Image src={discountImg} alt={'discountImg'}/>
         </div>
       </div>
 
